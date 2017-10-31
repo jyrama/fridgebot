11ca331d33f726256485d43f6c71db4910609ce4
BFlorry <bflorry@gmail.com>
2017-10-31 22:10:56 +0000

    Made some config changes

--- AlertBot.py
+++ AlertBot.py
@@ -7,7 +7,7 @@ buttonPin = 26
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(buttonPin,GPIO.IN)
 
-wait_seconds = 3
+wait_seconds = 120
 alert_wait = wait_seconds * 2
 
 t_end = time.time() + wait_seconds
@@ -15,10 +15,10 @@ alertfade_time = time.time() + alert_wait
 alert = False
 
 while True:
-  if not GPIO.input(buttonPin):
+  if GPIO.input(buttonPin):
     if time.time() >= t_end:
       print('Alert sent to mattermost')
-      Popen(['mattersend','--config', '/home/pi/techday/mattersend.conf', '-f', '/home/pi/techday/auki.txt'])
+      Popen(['mattersend','--config', '/home/pi/techday/fridgebot/mattersend.conf', '-f', '/home/pi/techday/fridgebot/auki.txt'])
       alert = True
       t_end = time.time() + wait_seconds
       alertfade_time = time.time() + alert_wait
@@ -27,5 +27,5 @@ while True:
     if alert == True and time.time() >= alertfade_time:
       print('Alert off')
       alert = False
-      Popen(['mattersend','--config', '/home/pi/techday/mattersend.conf', '-f', '/home/pi/techday/suljettu.txt'])
+      Popen(['mattersend','--config', '/home/pi/techday/fridgebot/mattersend.conf', '-f', '/home/pi/techday/fridgebot/suljettu.txt'])
   time.sleep(0.02)
