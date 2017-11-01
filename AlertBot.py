a04cf9468f3f4776578910e7c3ca344b412e17c1
Jyri Eerola <jyri.eerola@jrd.fi>
2017-11-01 02:49:39 +0200

    Refactor

--- AlertBot.py
+++ AlertBot.py
@@ -3,8 +3,10 @@ import time
 from subprocess import Popen, PIPE
 
 MATTERSEND_CONF = '/home/pi/techday/fridgebot/mattersend.conf'
+DOOR_CLOSED_FILE = '/home/pi/techday/fridgebot/suljettu.txt'
+DOOR_OPEN_FILE = '/home/pi/techday/fridgebot/auki.txt'
 
-irc = Popen(['ssh','-t','hacklab-irc'], stdin=PIPE)
+irc = Popen(['ssh', '-t', 'hacklab-irc'], stdin=PIPE)
 irc.stdin.write("pass freenode\nuser\nnick\n".encode())
 
 buttonPin = 26
@@ -20,22 +22,22 @@ alert = False
 
 while True:
     if GPIO.input(buttonPin):
-      if time.time() >= t_end:
-        print('Alert sent to mattermost', flush=True)
-        Popen(['mattersend', '--config', MATTERSEND_CONF, '-f',
-               '/home/pi/techday/fridgebot/auki.txt'])
-        irc.stdin.write("notice #hacklab.jkl :Jääkaapin ovi on jäänyt auki!\n".encode())
-        irc.stdin.flush()
-        alert = True
-        t_end = time.time() + wait_seconds
-        alertfade_time = time.time() + alert_wait
+        if time.time() >= t_end:
+            print('Alert sent to mattermost', flush=True)
+            Popen(['mattersend', '--config', MATTERSEND_CONF, '-f',
+                   DOOR_OPEN_FILE])
+            irc.stdin.write("notice #hacklab.jkl :Jääkaapin ovi on jäänyt auki!\n".encode())
+            irc.stdin.flush()
+            alert = True
+            t_end = time.time() + wait_seconds
+            alertfade_time = time.time() + alert_wait
     else:
         t_end = time.time() + wait_seconds
         if alert and (time.time() >= alertfade_time):
             print('Alert off', flush=True)
             alert = False
             Popen(['mattersend', '--config', MATTERSEND_CONF, '-f',
-                   '/home/pi/techday/fridgebot/suljettu.txt'])
+                   DOOR_CLOSED_FILE])
             irc.stdin.write("notice #hacklab.jkl :Jääkaapin ovi on suljettu, kiitos!\n".encode())
             irc.stdin.flush()
     time.sleep(0.02)
