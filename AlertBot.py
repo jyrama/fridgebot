822fed6fd07b46e7daef91b00514e35cbf7f34e9
Jyri Eerola <jyri.eerola@jrd.fi>
2018-03-31 04:03:43 +0300

    Load config from config.json and pass it to modules.

--- AlertBot.py
+++ AlertBot.py
@@ -1,19 +1,24 @@
 import RPi.GPIO as GPIO
 import time
 
+import json
 from glob import glob
 from importlib import import_module
 
-buttonPin = 26
+config = json.loads('config.json')
+
+buttonPin = config['button_pin']
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(buttonPin, GPIO.IN)
 
-wait_seconds = 120
-alert_wait = wait_seconds * 2
+wait_seconds = config['wait_seconds']
+alert_wait = config['alert_wait']
 
 t_end = time.time() + wait_seconds
 alertfade_time = time.time() + alert_wait
 alert = False
+
+mainloop_sleep = config['mainloop_sleep']
 
 # load sink modules, symlinked etc, from sinks folder
 sinks = [import_module(x.replace('/', '.')[:-3]) for x in glob('sinks/*.py')]
@@ -23,7 +28,11 @@ if len(sinks) == 0:
     print('No modules loaded!')
     sys.exit(1)
 
-    
+# Set config attribute for modules loaded
+for module in sinks:
+    module.config = config
+
+
 def send_notifications():
     for sink in sinks:
         sink.notify()
@@ -48,4 +57,4 @@ while True:
             print('Alert off', flush=True)
             send_thanks()
             alert = False
-    time.sleep(0.02)
+    time.sleep(mainloop_sleep)
