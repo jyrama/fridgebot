3c0a07c9d4aa90d9c6c31aa9c7f94a70e4696026
Jyri Eerola <jyri.eerola@jrd.fi>
2018-03-31 04:53:48 +0300

    Use new config scheme in hacklab-irc sink.

--- sinks-available/hacklab-irc.py
+++ sinks-available/hacklab-irc.py
@@ -1,15 +1,30 @@
 from subprocess import Popen, PIPE
 
-irc = Popen(['ssh', '-T', 'hacklab-irc'], stdin=PIPE)
-irc.stdin.write("pass freenode\nuser\nnick\n".encode())
-irc.stdin.write("notice #hacklab.jkl :Fridgebot is alive".encode())
-irc.stdin.flush()
+
+def init():
+    global config, irc, channel, notify_msg, thanks_msg
+
+    proxy = config['proxy']
+    channel = config['channel']
+    startup_msg = config['startup_msg']
+    startup_notice = "notice {} :{}".format(channel, startup_msg)
+    notify_msg = config['notify_msg']
+    thanks_msg = config['thanks_msg']
+
+    irc = Popen(['ssh', '-T', proxy], stdin=PIPE)
+    irc.stdin.write("pass freenode\nuser\nnick\n".encode())
+    irc.stdin.write(startup_notice.encode())
+    irc.stdin.flush()
+
 
 def notify():
-    irc.stdin.write("notice #hacklab.jkl :Pakastimen ovi on jäänyt auki!\n".encode())
+    global irc, channel
+    irc.stdin.write("notice {} :{}\n".format(channel, notify_msg).encode())
     irc.stdin.flush()
     print('Alert sent to IRC', flush=True)
 
+
 def thanks():
-    irc.stdin.write("notice #hacklab.jkl :Pakastimen ovi on suljettu, kiitos!\n".encode())
+    global irc, thanks_msg
+    irc.stdin.write("notice {} :{}\n".format(channel, thanks_msg).encode())
     irc.stdin.flush()
